d6896f521760ed8a1962b3fee7f6d67fa34b3fc1
Your Name <ikhlas505a@gmail.com>
2019-11-15 21:16:42 +0200

    ManegerBloodBank

---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+
+declare const $: any;
+declare interface RouteInfo {
+    path: string;
+    title: string;
+    icon: string;
+    class: string;
+}
+export const ROUTES: RouteInfo[] = [
+    { path: '/dashboard', title: 'Dashboard',  icon: 'dashboard', class: '' },
+    { path: '/user-profile', title: 'Hospital ',  icon:'hotel', class: '' },
+    { path: '/table-list', title: ' Blood Units',  icon:'content_paste', class: '' },
+    { path: '/notifications', title: 'Notifications',  icon:'notifications', class: '' },
+    { path: '/logout', title: 'Logout',  icon:'logout', class: 'active-pro' },
+];
+
+
+
+
+
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent implements OnInit {
+  menuItems: any[];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.menuItems = ROUTES.filter(menuItem => menuItem);
+  }
+  isMobileMenu() {
+      if ($(window).width() > 991) {
+          return false;
+      }
+      return true;
+  };
+}
